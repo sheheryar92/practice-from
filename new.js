f927cb1557aa2805450b3337c392672893026eb3
sheheryar92 <sheheryar.qazi92@gmail.com>
2021-07-18 23:13:57 +0500

    first commit

---
+++ new.js
@@ -0,0 +1,19 @@
+
+//  function textUp () {
+//  
+
+//     var finalResult = result.value.length;
+//     document.getElementById('text_count').innerHTML = finalResult;
+//  }
+
+
+// var a =  document.getElementById('getValue').value ;
+
+
+
+
+
+function submitValue() {
+    console.log('im clicked');
+    }
+    
